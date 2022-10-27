710c390a18bf39212d86051b15f4392f33aaf311
y5nw <37980625+y5nw@users.noreply.github.com>
2022-10-27 10:14:06 +0200

    Rewrite tlnew in C

---
+++ src/tlnew.c
@@ -0,0 +1,90 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <libgen.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+/* define out own warning functions here because err.h is not portable */
+#define warnx(fmt, ...) (fprintf(stderr, fmt, __VA_ARGS__))
+#define warn(fmt, ...) (warnx(fmt ": %s\n", __VA_ARGS__, strerror(errno)))
+
+/* masks for the permission of new files */
+#define DPERMMASK 0755
+#define FPERMMASK 0644
+
+/* Windows: the mkdir() function differs from the POSIX mkdir() */
+#ifdef _WIN32
+#define mkdir(fn, mode) mkdir(fn)
+#endif
+
+static int isdir = 0;
+
+static int create_parent(char *fn) {
+	struct stat st;
+	char *cfn = strdup(fn);
+	if (!cfn)
+		return -1;
+	char *dn = dirname(cfn);
+	if (stat(dn, &st)) {
+		int perm = create_parent(dn);
+		if (perm < 0) {
+			free(cfn);
+			return perm;
+		} else {
+			if (mkdir(dn, perm&DPERMMASK)) {
+				warn("Failed to create %s", dn);
+				free(cfn);
+				return -1;
+			} else {
+				free(cfn);
+				return perm;
+			}
+		}
+	} else {
+		if (st.st_mode & S_IFDIR) {
+			free(cfn);
+			return st.st_mode;
+		} else {
+			errno = ENOTDIR;
+			warn("%s", dn);
+			free(cfn);
+			return -1;
+		}
+	}
+}
+
+static void tlnew(char *fn) {
+	int perm = create_parent(fn);
+	if (perm < 0)
+		return (void) warnx("Failed to create parent directory for %s\n", fn);
+	if (isdir) {
+		if (mkdir(fn, perm & DPERMMASK))
+			warn("Failed to create directory %s", fn);
+	} else {
+		int fd = creat(fn, perm & FPERMMASK);
+		if (!fd)
+			warn("Failed to create regular file %s", fn);
+		close(fd);
+	}
+}
+
+int main(int argc, char **argv) {
+	int opt;
+	while ((opt = getopt(argc, argv, "-df")) != -1) {
+		switch (opt) {
+			case 'd':
+				isdir = 1;
+				break;
+			case 'f':
+				isdir = 0;
+				break;
+			case 1:
+				tlnew(optarg);
+				break;
+		}
+	}
+	return 0;
+}
